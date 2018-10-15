1db9bc1f9d14bd83193f04d755f88fc2d3c9ef49
Yahor Nikifarau <>
2018-10-14 20:33:22 -0600

    addBooks, editBook, singleton, storage

--- js/book.js
+++ js/book.js
@@ -1,13 +1,23 @@
 
-function Book(title, author, numPages, pubDate)
-{
+function Book(title, author, numPages, pubDate){
   this.title = title;
   this.author = author;
   this.numPages = numPages;
   this.pubDate = pubDate;
-};
+}
 
-Book.prototype.editBook = function(oBook)
-{
-
+Book.prototype.editBook = function(oBook){
+  //make sure the info is unique too
+  //get the properities in the oBook
+  var oBookProperties = oBook.entries();
+  for(var i = 0; i < oBookProperties.length; i++) {
+    if(this.indexOf(oBookProperties[i]) !== -1) {
+      oBookProperties[i] = undefined;
+    }
+  }
+this.title = oBook.title || this.title;
+this.author = oBook.author || this.author;
+this.numPages = oBook.numPages || this.numPages;
+this.pubDate = oBook.pubDate || this.pubDate;
+return this;
 };
--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,17 @@
+(function() {//SINGLETON
+  var instance;
+  //console.log(instance);
+  Library = function() {
+    if (instance) { //if a instance of library already exists this will point the newly made library to the Singleton instance
+      //console.log('already made an instance');
+      return instance;
+    }
+    //console.log('new instance made');
+    this.bookShelf = [];
+    instance = this; //if a instance of library does not yet exist this will get and set the instance name for the new library
+  };
+})();
+
 function Library()
 {
   this.bookShelf = new Array();
@@ -5,29 +19,39 @@ function Library()
 
 Library.prototype.addBook = function(book){
     for(var i=0; i < this.bookShelf.length; i++){
-      if(this.bookShelf[i].title==book.title){
-      return false;
-        }
+      /*compare book title*/
+      if(this.bookShelf[i].title===book.title.toLowerCase().trim()){
+        return false;
+      }
+
     }
     this.bookShelf.push(book);
     return true;
+
 };
 
 Library.prototype.removeBookByAuthorName = function(author){
+
+var x=false;
       for(var i=0; i < this.bookShelf.length; i++){
           if(this.bookShelf[i].author==author){
-            this.bookShelf.splice(i, 0);
-            return true;
+// The splice() method changes the contents of an array by removing existing elements
+            this.bookShelf.splice(i, 1); //gLibrary.bookShelf.splice(i, 1);
+            i--;
+            x=true;
+
           }
       }
-  return false;
+  return x;
 };
 
 Library.prototype.removeBookByTitle = function(title){
   for(var i=0; i < this.bookShelf.length; i++){
     if(this.bookShelf[i].title == title){
+ // The splice() method changes the contents of an array by removing existing elements
           this.bookShelf.splice(i,1);
           return true;
+          //break; never happend ,only before true statment;
     }
   }
   return false;
@@ -35,6 +59,7 @@ Library.prototype.removeBookByTitle = function(title){
 
 Library.prototype.getRandomBook = function(){
     if(this.bookShelf.length>0){
+      /* create index*/
       var index =Math.floor(Math.random() * this.bookShelf.length);
         return this.bookShelf[index];
     }else{
@@ -46,7 +71,8 @@ Library.prototype.getRandomBook = function(){
 Library.prototype.getBooksByTitle = function(title){
 	var newArray = new Array();
 	for(var i=0; i < this.bookShelf.length; i++){
-		if(this.bookShelf[i].title.indexOf(title) >= 0){
+    /*returns the first index at which a given element can be found in the array*/
+		if(this.bookShelf[i].title.toLowerCase().indexOf(title) >= 0){
 			newArray.push(this.bookShelf[i]);
 		}
 	}
@@ -56,16 +82,43 @@ Library.prototype.getBooksByTitle = function(title){
 Library.prototype.getBooksByAuthor = function(authorName){
 	var newArray = new Array();
 	for(var i=0; i < this.bookShelf.length; i++){
-		if(this.bookShelf[i].author.indexOf(authorName) >= 0){
+    /*returns the first index at which a given element can be found in the array*/
+		if(this.bookShelf[i].author.toLowerCase().indexOf(authorName) >= 0){
 			newArray.push(this.bookShelf[i]);
 		}
 	}
 	return newArray;
 };
 
+Library.prototype.addBooks = function(bookShelf) {
+  //empty counter to add books to
+	var booksAdded = 0;
+	for (var i = 0; i < bookShelf.length; i++) {
+  	if (this.addBook(bookShelf[i])){
+			booksAdded ++;
+		}
+  }
+  console.log("Books Added: " + booksAdded);
+  return booksAdded;
+};
+
+Library.prototype.getAuthors = function() {
+
+  var unique = {};//O(1) an object, each occurence being tracked is added as a key, final set of keys represents unique occurences
+  var distinct = [];
+  for(var i in this.bookShelf) {
+    if(typeof(unique[this.bookShelf[i].author]) == "undefined") {//if author from the array isnt in unique
+    distinct.push(this.bookShelf[i].author);//push it into distinct
+    }
+    unique[this.bookShelf[i].author] = 0;//first time sees unique author assigns number 0 as value to key of author name - value can be anything
+   }
+  return distinct;
+};
+
 
 Library.prototype.getRandomAuthorName = function(author){
     if(this.bookShelf.length>0){
+      /*create index*/
       var index =Math.floor(Math.random() * this.bookShelf.length);
         return this.bookShelf[index].author;
     }else{
@@ -73,21 +126,40 @@ Library.prototype.getRandomAuthorName = function(author){
     }
 };
 
-Library.prototype.getAuthors = function(authorName){
-	 var newArray = new Array();
-	 for(var i=0; i < this.bookShelf.length; i++){
-     newArray.push(this.bookShelf[i].author);
-   }
-   return newArray;
+//// book localStorage
+Library.prototype.borkStorage = function(){
+  var r = confirm("Continue with localStorage borkening?");
+  if (r){
+    localStorage.clear("overStockShelf");
+    alert("Successfully borked localStorage.");
+  }
+};
+
+  Library.prototype.createLocalStorage = function(){
+  localStorage.setItem('bookshelf', JSON.stringify(this.bookShelf));
+  for (var i = 0, len = localStorage.length; i < len; ++i) {
+    console.log("iterating through all local storage keys and values : " + localStorage.getItem(localStorage.key(i)));
+  }
+};
+
+Library.prototype.getLocalStorage = function(){
+  var bookShelf = [];
+  var parsedBookShelf = JSON.parse(localStorage.setItem('bookshelf'));
+  for (var i = 0; i < parsedBookShelf.length; ++i) {
+    bookShelf.push(new Book(parsedBookShelf[i].title , parsedBookShelf[i].author , parsedBookShelf[i].numPages , parsedBookShelf[i].pubDate));
+  }
+  return bookShelf;
 };
 
 
+//egor
 
-
-document.addEventListener("DOMContentLoaded", function(e){
+ document.addEventListener("DOMContentLoaded", function(e){
  window.gLibrary = new Library();
+
+
  book1 = new Book("IT", "SK", 200,"Jan 1th");
- book2 = new Book("Drama", "SK1", 300,"Jan 15th");
+ book2 = new Book("Drama", "SK", 300,"Jan 15th");
  book3 = new Book("IT2", "Garry Poter", 30,"Okt 66th");
  book4 = new Book("Comedy", "Garry", 40,"Sep 66th");
  book5 = new Book("Comedy1", "SK2", 130,"March 66th");
@@ -97,10 +169,6 @@ document.addEventListener("DOMContentLoaded", function(e){
  book9 = new Book("Comedy5", "Egor4", 130,"March 66th");
  book10 = new Book("Comedy6", "Egor5", 130,"March 66th");
 
-
-
-
-
  gLibrary.addBook(book1);
  gLibrary.addBook(book2);
  gLibrary.addBook(book3);
@@ -109,9 +177,6 @@ document.addEventListener("DOMContentLoaded", function(e){
  gLibrary.addBook(book6);
  gLibrary.addBook(book7);
  gLibrary.addBook(book8);
- gLibrary.addBook(book9);
- gLibrary.addBook(book10);
-
-
-
+ // gLibrary.addBook(book9);
+ // gLibrary.addBook(book10);
 });
